0be875e365339fc89acf48774efa81d8b5680e72
mahtabmardani88 <128320457+mahtabmardani88@users.noreply.github.com>
2024-02-09 16:55:12 +0100

    javascript advance

---
+++ JS.js
@@ -0,0 +1,36 @@
+// ============================================================
+// exercises 1
+// function filterRanger (arr,a,b) {
+//   let see=arr.filter(x => x.a && x <= b);
+// }
+// let arr=[5,3,8,1 ];
+// let filtered = filterRanger(arr,1,4);
+// console.log(filtered);
+
+// exercises 2
+// let john = { name: "John", age: 25 }
+// let pete = { name: "Pete", age: 30 } 
+// let mary = { name: "Mary", age: 28 }
+
+// let users = [ john, pete, mary ];
+
+// let names =users.map(x => x.name);
+// console.log(names);
+
+// Exercises 3
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [ john, pete, mary ];
+
+// function getAverageAge(users) {
+//     if (users.length === 0) {
+//       return 0; 
+//     }
+//     let ages = users.reduce((sum, user) => sum + user.age, 0);
+//     let average = ages / users.length;
+//     return average;
+//   }
+//   console.log(getAverageAge(arr));
+  
